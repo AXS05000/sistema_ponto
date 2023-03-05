d2085cd7b62bf36962c0e5b969963efd7b586577
Alex Sobreira <axssxs00@gmail.com>
2023-03-05 16:51:39 -0300

    Version - 05/03/2023 - 16:51

--- ponto_eletronico/admin.py
+++ ponto_eletronico/admin.py
@@ -1,26 +1,32 @@
 from django.contrib import admin
 
-from .models import Competencia, Departamento, Funcionario, Ponto
+from .models import Competencia, Departamento, Funcionario, Ponto, RegraDePonto
 
 
 class DepartamentoAdmin(admin.ModelAdmin):
     list_display = ('nome',)
 
 
+@admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'departamento', 'usuario')
 
 
+@admin.register(Competencia)
 class CompetenciaAdmin(admin.ModelAdmin):
     list_display = ('tipo_comp', 'mes', 'ano', 'data_inicio', 'data_fim')
 
 
+@admin.register(Ponto)
 class PontoAdmin(admin.ModelAdmin):
     listdisplay = ('funcionario', 'data', 'competencia',
                    'entrada', 'entrada_almoco', 'saida_almoco', 'saida')
 
 
+@admin.register(RegraDePonto)
+class RegraDePontoAdmin(admin.ModelAdmin):
+    listdisplay = ('nome', 'toleranciaentrada', 'toleranciasaida',
+                   'horaextraautorizada', 'periodohoranoturnainicio', 'periodohoranoturna_fim')
+
+
 admin.site.register(Departamento, DepartamentoAdmin)
-admin.site.register(Funcionario, FuncionarioAdmin)
-admin.site.register(Ponto, PontoAdmin)
-admin.site.register(Competencia, CompetenciaAdmin)
